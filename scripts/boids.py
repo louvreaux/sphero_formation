Fixed init state and observation calculation.

scripts/boids.py
@@ -242,7 +242,7 @@ class Boid(object):
         # Force is not limited so this rule has highest priority.
         return main_direction + safety_direction
 
-    def compute_velocity(self, my_agent, nearest_agents, avoids, reset=False):
+    def compute_velocity(self, my_agent, nearest_agents, avoids, reset=False, action=-1):
         """Compute total velocity based on all components."""
 
         # While waiting to start, send zero velocity and decrease counter.
@@ -300,7 +300,7 @@ class Boid(object):
             
             # OVO JE NOVO DODANO
             #self.velocity.limit(self.max_speed)
-            #self.velocity = Vector2(x = 1.0, y = 0.0)
+            #self.velocity = Vector2(x = -1 * self.max_speed, y = 0.0)
             self.velocity.normalize()
             self.velocity = self.velocity * self.max_speed
 
@@ -322,7 +322,6 @@ class Boid(object):
             vel = Twist()
 
             if reset == True:
-                action = randint(0, 7)
                 theta = action * 45.0 * (np.pi/180.0)
                 new_vel = np.array([-math.sin(-theta), math.cos(-theta)]) * self.max_speed
                 vel.linear.x = new_vel[0]

scripts/robot_envs/sphero_env.py
@@ -76,19 +76,12 @@ class SpheroEnv(robot_stage_env.RobotStageEnv):
         dist_array = np.array([])
         closest_agent_dist = np.inf
 
-        # Get our agent direction angle
-        temp_var = get_agent_velocity(my_agent)
-        temp_var.normalize()
-        self.agent_steer = np.arccos(temp_var.y) # Angle between [0, 1] and normalized agent steering vector -> we only need y component
-        if temp_var.x < 0:
-           self.agent_steer += 2*np.pi - self.agent_steer                # We need to take into account that angle between vectors is always the smallest one
-
         # Init observation variables
         self.closest_obstacles = np.ones((10,2))                         # Relative position of 10 closest obstacles
         self.closest_neighbour = np.ones(2)                              # Relative position of closest neighbour
         self.flock_pose = np.ones(2)                                     # Relative position of flock
-        self.flock_vel =  np.ones(1)                                     # Number of neighbours inside search radius
-        self.num_of_nearest_agents = len(nearest_agents)
+        self.flock_vel =  1.0                                            # Flock direction angle
+        self.num_of_nearest_agents = len(nearest_agents)                 # Number of neighbours
 
         if nearest_agents:
             for agent in nearest_agents:
@@ -104,7 +97,6 @@ class SpheroEnv(robot_stage_env.RobotStageEnv):
             # noise = np.random.normal(0.0, 0.03)     # Adding some noise for better generalization
 
             # Get closest neighbour observation
-            # self.closest_neighbour = self.closest_neighbour+ np.array([noise, noise])            # [X, Y]
             # rospy.logerr("CLOSEST NEIGHBOUR X,Y: " + str(self.closest_neighbour))
             temp_dist = np.sqrt(self.closest_neighbour[0]**2 + self.closest_neighbour[1]**2)
             temp_angle = np.arccos(self.closest_neighbour[1] / temp_dist)
@@ -143,6 +135,15 @@ class SpheroEnv(robot_stage_env.RobotStageEnv):
                 counter += 1
                 if counter >= 10:
                     break
+
+        # Get our agent direction angle
+        temp_var = get_agent_velocity(my_agent)
+        temp_var.normalize()
+
+        temp = np.arccos(temp_var.y) # Angle between [0, 1] and normalized agent steering vector -> we only need y component
+        if temp_var.x < 0:
+           temp = 2*np.pi - temp # We need to take into account that angle between vectors is always the smallest one
+        self.agent_steer = temp
 
     # Methods that the TrainingEnvironment will need to define here as virtual
     # because they will be used in RobotStageEnv GrandParentClass and defined in the
@@ -184,7 +185,10 @@ class SpheroEnv(robot_stage_env.RobotStageEnv):
         rospy.logdebug("Sphero Twist>>" + str(velocity))
         self._cmd_vel_pub.publish(velocity)
 
-        # TU JE BILA FUNKCIJA KOJA CEKA DA ROBOT PROMIJENI POZICIJU TEMELJEM AKCIJE
-    def get_callback(self):
-
+    def get_callback(self, curr_action, curr_flock_vel):
+        # This is to make sure we observe the action our agent took
+        # Flock velocity check is added because steer action can be the same as last time
+        if curr_action != -1.0:
+            while abs(self.agent_steer - curr_action) > 0.001 or curr_flock_vel == self.flock_vel:
+                pass
         return self.agent_steer, self.closest_neighbour, self.flock_pose, self.flock_vel, self.closest_obstacles, self.num_of_nearest_agents

scripts/sphero_dqlearn.py
@@ -40,7 +40,7 @@ class Agent:
         self.batchSize = 64  # Size of a miniBatch(64)
         self.learnStart = 50000 # Start to train model from this step(100000)
         self.memory = deque(maxlen=200000)  # Main memory to keep batches
-        self.timeOutLim = 200  # Maximum step size for each episode(1400)
+        self.timeOutLim = 1500  # Maximum step size for each episode(1400)
         self.savePath = '/home/louvreaux/bzvz/src/sphero_formation/training_results/'  # Model save path
 
         self.onlineModel = self.initNetwork()
@@ -177,13 +177,16 @@ if __name__ == '__main__':
     for episode in range(agent.loadEpisodeFrom + 1, agent.episodeCount):
         done = False
         state = env.reset()
+        # rospy.logerr("STATE: " + str(state))
         score = 0
         total_max_q = 0
         
         for step in range(1,999999):
-
+            # print("STEP: " + str(step))
             action = agent.calcAction(state)
+            # rospy.logerr("ACTION: " + str(action))
             nextState, reward, done, info = env.step(action)
+            # rospy.logerr("NEXT STATE: " + str(nextState))
             env.pause()
 
             if score+reward > 10000 or score+reward < -10000:
@@ -201,6 +204,7 @@ if __name__ == '__main__':
             score += reward
             state = nextState
 
+            # Display information by steps
             avg_max_q_val_text = "Avg Max Q Val:{:.2f}  | ".format(np.max(agent.qValue))
             reward_text = "Reward:{:.2f}  | ".format(reward)
             action_text = "Action:{:.2f}  | ".format(action)
@@ -227,7 +231,7 @@ if __name__ == '__main__':
 
                 avg_max_q = total_max_q / step
 
-                # Infor user
+                # Display information by episodes
                 m, s = divmod(int(time.time() - startTime), 60)
                 h, m = divmod(m, 60)
 

scripts/task_envs/sphero/sphero_world1.py
@@ -43,7 +43,7 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 		# Define action and observation space
 		self.action_space = spaces.Discrete(int(360.0/self.angle_quant_step))
 
-		self.observation_space = spaces.Box(low=np.array([-2*np.pi, -2*np.pi, 0.0, -2*np.pi, 0.0]), 
+		self.observation_space = spaces.Box(low=np.array([-2*np.pi, 0.0, 0.0, 0.0, 0.0]), 
 											high=np.array([2*np.pi, 2*np.pi, self.rel_pose_max, 2*np.pi, self.rel_pose_max]), 
 											dtype=np.float64)
 
@@ -55,10 +55,16 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 		rospy.logdebug("OBSERVATION SPACES TYPE===>"+str(self.observation_space))
 
 		self.cumulated_steps = 0.0
-		self.last_obs = np.array([])
 
 		# Here we will add any init functions prior to starting the MyRobotEnv
 		super(SpheroWorldEnv, self).__init__()
+
+		self.curr_action = -1.0
+		self.curr_flock_steer = -1.0
+		
+		# Init first state while starting learning
+		self.first_state = True
+
 
 	def _set_init_pose(self):
 		"""Sets the Robot in its init pose
@@ -86,13 +92,18 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 		based on the action number given.
 		:param action: The action integer that set s what movement to do next.
 		"""
-		# rospy.logerr("ACTION: " + str(action))
-		
+
 		rospy.logdebug("Start Set Action ==>"+str(action))
-		# We convert the actions to speed movements to send to the parent class CubeSingleDiskEnv
+
+		# We convert the actions to speed movements
+		#rospy.logerr("ACTION: " + str(action*self.angle_quant_step))
 		theta = action * self.angle_quant_step * (np.pi/180.0)
-		new_vel = np.array([-sin(-theta), cos(-theta)]) * self.vel_max # efektivno rotiramo vektor [0, 1] u desno (jer tako gledamo i kut gibanja jata)
+		new_vel = np.array([-sin(-theta), cos(-theta)]) * self.vel_max # We rotate vector [0, 1] to the right
+
+		# Used to check if agent responded to new action
+		self.curr_action = theta
 		
+		# Publish velocity
 		send_velocity = Twist()
 		send_velocity.linear.x = new_vel[0]
 		send_velocity.linear.y = new_vel[1]
@@ -106,31 +117,42 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 		:return:
 		"""
 		rospy.logdebug("Start Get Observation ==>")
-		agent_steer, closest_neighbour, flock_pose, flock_steer, closest_obst, num_of_neighbours = self.get_callback()
+		agent_steer, closest_neighbour, flock_pose, flock_steer, closest_obst, num_of_neighbours = self.get_callback(self.curr_action, self.curr_flock_steer)
+		self.curr_flock_steer = flock_steer
 
-		if num_of_neighbours > 0:
+		# If our agent didn't loose the flock, examine observations
+		if num_of_neighbours > 0 or self.first_state == True:
 			self._episode_done = False
 
-			agent_steer = agent_steer * 180.0/np.pi
+			# We discretize steering because of reward calculation
 			flock_steer = self.observation_quantization(flock_steer * 180.0/np.pi, 0.0, 360.0, self.angle_quant_step)
+			# rospy.logerr("FLOCK: " + str(flock_steer))
 			if flock_steer == 360.0:
 				flock_steer = 0.0
-			steer_angle_diff = agent_steer - flock_steer
-			steer_angle_diff = steer_angle_diff * np.pi / 180.0
 
+			# Calculate relative diference between agent and flock direction
+			steer_angle_diff = agent_steer - flock_steer * np.pi / 180.0
+			if abs(steer_angle_diff) <= 0.001:
+				steer_angle_diff = 0.0
+			# rospy.logerr("ANGLE DIFF: " + str(steer_angle_diff*180.0/np.pi))
+
+			# If agent is too close to neighbour, finish episode
 			if closest_neighbour[1] <= self.too_close:
 				self._episode_done = True
-				# self.crash = True
 
 			observations = np.array([steer_angle_diff, closest_neighbour[0], closest_neighbour[1], flock_pose[0], flock_pose[1]])
 			self.last_obs = observations
+
+			self.first_state = False
+			
+		# Else, return last known observation
 		else:
 			self._episode_done = True
 			observations = self.last_obs
 		rospy.logdebug("Observations==>"+str(observations))
 		rospy.logdebug("END Get Observation ==>")
 		rospy.logwarn(observations)
-		# rospy.logerr("OBSERVATIONS: " + str(observations))
+		#rospy.logerr("OBSERVATIONS: " + str(observations))
 
 		return observations
 		
@@ -148,7 +170,7 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 
 		if not done:
 
-			# NEMOJ ICI PREBLIZU SUSJEDU
+			# Punish being too close to neighbour
 			if observations[2] <= 0.3 and observations[2] > 0.1:
 				reward += 25 * observations[2] - 7.5
 			elif observations[2] <= 0.1:
@@ -156,13 +178,13 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 			# temp = reward
 			# rospy.logerr("CLOSEST NEIGHBOUR REWARD: " + str(reward))
 
-			# OVO JE DA NE BUDE BLIZU PREPRECI
+			# Punish being close to an obstacle
 			# arr = observations[4]
 			# for i in range(0, len(arr)):
 			#     if sqrt(arr[i][0]**2 + arr[i][1]**2) <= self.avoid_radius_q:
 			#         reward -=  5.0
 
-			# DRZI SE BLIZU JATA
+			# Reward being close to flock
 			if observations[4] <= 0.6 and observations[4] > 0.1:
 				reward += -10.0 * observations[4] + 6.0
 			elif observations[4] <= 0.1:
@@ -170,12 +192,12 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 			# rospy.logerr("FLOCK REWARD: " + str(reward - temp))
 			# temp = reward
 
-			# OVO JE DA IMA ISTI SMJER
-			if observations[0] != 0.0:
-				reward -= 3.0
+			# Punish not going the same way as flock
+			if abs(observations[0]) != 0.0:
+				reward -= 5.0 / (2 * np.pi) * abs(observations[0])
 			# rospy.logerr("STEER REWARD: " + str(reward - temp))
 		else:
-			 reward += self.end_episode_points
+			 reward -= 10.0
 
 		rospy.logdebug("reward=" + str(reward))
 		self.cumulated_reward += reward
@@ -186,6 +208,8 @@ class SpheroWorldEnv(sphero_env.SpheroEnv):
 		return reward
 
 	def observation_quantization(self, value, min_value, max_value, qstep):
+		""" Used for discretization of observations, usually needed for
+			discrete algorithms, such as q-learning """
 		if np.greater(value, max_value):
 			value = max_value
 		elif np.less(value, min_value):